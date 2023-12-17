ec84a14ad8b5e6db6f3087e82fa226cc224c0702
SonuAkhtar <sonua981@gmail.com>
2023-12-17 18:44:30 +0530

    header complete

--- src/components/MenuDesk/MenuDesk.jsx
+++ src/components/MenuDesk/MenuDesk.jsx
@@ -1,19 +1,13 @@
 import React, { useState } from "react";
 
+// imported Data
+import { menuDeskData } from "../../../appData";
+
 // imported CSS
 import "./menuDesk.css";
 
 const MenuDesk = ({ show, setShow }) => {
   const [menuItem, setMenuItem] = useState("three");
-
-  const menuDeskItems = [
-    { id: 0, class: "one", number: "01", name: "About", href: "#about" },
-    { id: 1, class: "two", number: "02", name: "Skills", href: "#skills" },
-    { id: 2, class: "three", number: "03", name: "Hero", href: "#" },
-    { id: 3, class: "four", number: "04", name: "Work", href: "#work" },
-    { id: 4, class: "five", number: "05", name: "Hobbies", href: "#hobbies" },
-    { id: 5, class: "six", number: "06", name: "Contact", href: "#contact" },
-  ];
 
   const handleMenuClick = (val) => {
     setMenuItem(val);
@@ -25,7 +19,7 @@ const MenuDesk = ({ show, setShow }) => {
   return (
     <div className={`menuDesk_container ${show && "show"}`}>
       <div className={`menuDesk_circle ${menuItem}`}>
-        {menuDeskItems.map((item, i) => (
+        {menuDeskData.map((item, i) => (
           <div
             key={i}
             className={`menuDesk_stripe ${item.class}`}
--- src/components/MenuMobile/MenuMobile.jsx
+++ src/components/MenuMobile/MenuMobile.jsx
@@ -1,54 +1,13 @@
 import React, { useState } from "react";
+
+// imported Data
+import { menuMobileData } from "../../../appData";
+
+// imported CSS
 import "./menuMobile.css";
 
 const MenuMobile = ({ expand, setExpand }) => {
   const [menuItem, setMenuItem] = useState("one");
-
-  const menuMobileItems = [
-    {
-      id: 0,
-      class: "one",
-      icon: "fas fa-home",
-      name: "Hero",
-      href: "#",
-    },
-    {
-      id: 1,
-      class: "two",
-      icon: "fas fa-user",
-      name: "About",
-      href: "#about",
-    },
-    {
-      id: 2,
-      class: "three",
-      icon: "fas fa-file-code",
-      name: "Skills",
-      href: "#skills",
-    },
-
-    {
-      id: 3,
-      class: "four",
-      icon: "fas fa-home",
-      name: "Work",
-      href: "#work",
-    },
-    {
-      id: 4,
-      class: "five",
-      icon: "fas fa-camera",
-      name: "Hobbies",
-      href: "#hobbies",
-    },
-    {
-      id: 5,
-      class: "six",
-      icon: "fas fa-phone",
-      name: "Contact",
-      href: "#contact",
-    },
-  ];
 
   const handleMenuClick = (val) => {
     setMenuItem(val);
@@ -58,7 +17,7 @@ const MenuMobile = ({ expand, setExpand }) => {
   return (
     <div className={`menuMobile_container ${expand && "expand"}`}>
       <ul className={`${expand && "expand"}`}>
-        {menuMobileItems.map((item, i) => (
+        {menuMobileData.map((item, i) => (
           <li key={i} onClick={() => handleMenuClick(item.class)}>
             <a
               href={item.href}
